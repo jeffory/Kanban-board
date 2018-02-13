6eba719d944a0f7b1e1caa6e986fa5007af59cc8
Keith McGahey <jeffory@c0d.in>
2018-02-13 22:52:44 +1000

    Clean out old code

--- src/store/index.js
+++ src/store/index.js
@@ -7,17 +7,5 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   modules: {
     tasks
-  },
-  mutations: {
-    updateColumnTitle (state, data) {
-      state.columnNames[data.id] = data.newTitle
-    },
-    updateTasks (state, data) {
-      state.tasks = data.tasks
-    }
   }
 })
-
-// state: {
-//  columnNames: ['List 1', 'List 2', 'List 3', 'List 4'],
-// },
