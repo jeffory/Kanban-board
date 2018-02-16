da631e8b7c241a62acb9505820587f3e41ed0472
Keith McGahey <jeffory@c0d.in>
2018-02-16 10:49:14 +1000

    Add more columns to tasks data and remove task method

--- src/api/tasks.js
+++ src/api/tasks.js
@@ -1,3 +1,5 @@
+import _remove from 'lodash/remove'
+
 /**
  * Mocking client-server processing
  */
@@ -7,23 +9,39 @@ const _tasks = {
     [
       {
         id: 0,
-        description: 'Buy some milk'
+        description: 'Buy some milk',
+        status: 0,
+        details: '',
+        created: new Date().setTime(1518730711109),
+        updated: new Date().setTime(1518730711109)
       },
       {
         id: 1,
-        description: 'Switch out draggable experiment with a sortable option'
+        description: 'Switch out draggable experiment with a sortable option',
+        status: 0,
+        details: '',
+        created: new Date().setTime(1518730721811),
+        updated: new Date().setTime(1518730721811)
       }
     ],
     [
       {
         id: 2,
-        description: 'Buy eggs'
+        description: 'Buy eggs',
+        status: 0,
+        details: '',
+        created: new Date().setTime(1518730736484),
+        updated: new Date().setTime(1518730736484)
       }
     ],
     [
       {
         id: 3,
-        description: 'Buy spices'
+        description: 'Buy spices',
+        status: 0,
+        details: '',
+        created: new Date().setTime(1518730747699),
+        updated: new Date().setTime(1518731398647)
       }
     ],
     []
@@ -36,12 +54,41 @@ export default {
   getTasks (callback) {
     setTimeout(() => callback(_tasks), 100)
   },
-  addTask (data, callback) {
+  addTask (task, callback) {
     setTimeout(() => {
-      _tasks.items[data.columnIndex].push({id: autoIncrementID, description: data.text})
+      _tasks.items[task.columnIndex].push({
+        id: autoIncrementID,
+        description: task.text,
+        status: 0,
+        details: '',
+        created: new Date(),
+        updated: new Date()
+      })
       autoIncrementID++
 
       callback(_tasks)
+    }, 100)
+  },
+  updateTask (task, callback) {
+    setTimeout(() => {
+      _tasks.items[task.columnIndex].push({
+        id: autoIncrementID,
+        description: task.text,
+        status: 0,
+        details: '',
+        created: new Date(),
+        updated: new Date()
+      })
+      autoIncrementID++
+
+      callback(_tasks)
+    }, 100)
+  },
+  removeTask (taskID, callback) {
+    setTimeout(() => {
+      _remove(_tasks.items, (task) => task.id === taskID)
+
+      callback()
     }, 100)
   }
 }
