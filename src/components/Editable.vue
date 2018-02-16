d06c5b14dad0fb489c666f87c98e49c09f3989d5
Keith McGahey <jeffory@c0d.in>
2018-02-16 10:46:47 +1000

    Add optional placeholder to Editable component

--- src/components/Editable.vue
+++ src/components/Editable.vue
@@ -1,12 +1,15 @@
 <template>
-  <span class="is-editable" contenteditable="true" v-text="text" @blur="updated" @keydown.enter="enterPressed"></span>
+  <span class="editable" contenteditable="true" v-text="text" @blur="updated" @keydown.enter="enterPressed" :data-placeholder="placeholder"></span>
 </template>
 
 <script>
 export default {
-  props: ['id', 'text', 'no-newlines'],
+  props: ['id', 'text', 'no-newlines', 'placeholder'],
   methods: {
     updated (event) {
+      // Trim last <br /> that some browsers add, so the placeholder can work
+      this.text = this.text.replace(/<br[^>]*\/?>\n*$/gim, '')
+
       // If text is no different to the original
       if (event.target.innerText === this.text) {
         return
@@ -31,24 +34,36 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.is-editable {
+.editable {
   display: inline-block;
   box-sizing: border-box;
   min-height: 1.6em;
-  padding: .35em .5em .25em;
+  padding: .35rem .5rem .25rem;
+  width: 100%;
+
+  &:hover {
+    background-color: hsl(0, 0%, 97%);
+    // padding: .25em .5em .20em;
+  }
+
+  &:empty:before {
+    color: #bbb;
+    content: attr(data-placeholder);
+    display: block;
+    font-style: italic;
+  }
 }
 
-.is-editable:not(:focus):not(:active) {
+.editable:not(:focus):not(:active) {
   cursor: pointer;
 }
 
-.is-editable:active,
-.is-editable:focus {
+.editable:active,
+.editable:focus {
   background-color: #fff;
   border: 1px solid #ccc;
   color: #555;
   outline: none;
-  padding: .25em .5em .20em;
-  width: 100%;
+  padding: .28rem .5rem .20rem;
 }
 </style>
