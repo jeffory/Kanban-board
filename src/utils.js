7fad568420fa43ab05bc49618d1d957307ded2f4
Keith McGahey <jeffory@c0d.in>
2018-02-19 13:59:35 +1000

    WIP: Add a deep remove function
    Needs to be swapped out with a better function, just putting a temporary function in that passes to test with other code

--- src/utils.js
+++ src/utils.js
@@ -33,5 +33,29 @@ export default {
     }
 
     return result
+  },
+  deepRemove (array, predicate) {
+    let {index} = this.deepFind(array, predicate)
+
+    // TODO: Temporary code - find a better way, without eval
+    switch (index.length) {
+      case 1:
+        array.splice(index[0], 1)
+        break
+      case 2:
+        array[index[0]].splice(index[1], 1)
+        break
+      case 3:
+        array[index[0]][index[1]].splice(index[2], 1)
+        break
+      case 4:
+        array[index[0]][index[1]][index[2]].splice(index[3], 1)
+        break
+      case 5:
+        array[index[0]][index[1]][index[2]][index[3]].splice(index[4], 1)
+        break
+    }
+
+    return array
   }
 }
